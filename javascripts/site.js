0a55bd1071f47d94271c3197a961a6f8ea209b47
Wright Rickman <wrightrickman@gmail.com>
2014-02-17 23:52:21 -0500

    Changed CNAME. Added pages.

--- javascripts/site.js
+++ javascripts/site.js
@@ -1,17 +1,83 @@
-var turn = function(){
-	var cube = $('#cube');
-	cube.animate({
-		"transform": "rotateY(75deg) rotate(75deg)",
-		"-webkit-transform": "rotateY(75deg) rotateX(75deg)",
-		"-moz-transform": "rotateY(75deg) rotateX(75deg)",
-		"-ms-transform": "rotateY(75deg) rotateX(75deg)"
-	}, 500)
+var showHome = function(){
+	$('#wrapper').empty()
+	$('#wrapper').hide().append($('#home_template').html()).fadeIn(300)
+
+	$('#project_link').on("click", function(){
+		showProject();
+	})
+
+	$('#resume_link').on("click", function(){
+		showResume();
+	})
+
+	$('#contact_link').on("click", function(){
+		showContact();
+	})
+}
+
+var showResume = function(){
+	$('#wrapper').empty()
+	$('#wrapper').hide().append($('#resume_template').html()).fadeIn(300)
+
+	$('#home_link').on("click", function(){
+		showHome();
+	})
+
+	$('#project_link').on("click", function(){
+		showProject();
+	})
+
+	$('#contact_link').on("click", function(){
+		showContact();
+	})
+}
+
+var showContact = function(){
+	$('#wrapper').empty()
+	$('#wrapper').hide().append($('#contact_template').html()).fadeIn(300)
+
+	$('#home_link').on("click", function(){
+		showHome();
+	})
+
+	$('#project_link').on("click", function(){
+		showProject();
+	})
+
+	$('#resume_link').on("click", function(){
+		showResume();
+	})
+}
+
+var showProject = function(){
+	$('#wrapper').empty()
+	$('#wrapper').hide().append($('#project_template').html()).fadeIn(300)
+
+	$('#home_link').on("click", function(){
+		showHome();
+	})
+
+	$('#resume_link').on("click", function(){
+		showResume();
+	})
+
+	$('#contact_link').on("click", function(){
+		showContact();
+	})
 }
 
 $(function(){
-	var turnButton = $('#turn');
-	turnButton.on("click", function(){
-		turn();
-		console.log("Works")
+	$('#wrapper').append($('#home_template').html())
+
+	$('#project_link').on("click", function(){
+		showProject();
+	})
+
+	$('#resume_link').on("click", function(){
+		showResume();
+	})
+
+	$('#contact_link').on("click", function(){
+		showContact();
 	})
 })
