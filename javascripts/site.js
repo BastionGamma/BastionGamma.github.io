f77d70f918a9abe92465304e0d576cdc720d78b0
BastionGamma <wrightrickman@gmail.com>
2014-08-03 19:18:27 -0400

    actually fixed parallax on phone

--- javascripts/site.js
+++ javascripts/site.js
@@ -3,7 +3,9 @@ $(function(){
 	redrawDotNav();
 
 	$(window).bind('scroll', function(e){
-		parallaxScroll();
+		if(!/Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+			parallaxScroll();
+		}
 		redrawDotNav();
 	});
 
