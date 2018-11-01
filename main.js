7b0e7cb00a9e7ab14f7da1ab62b10c85bd86bc43
WrightRickman <wrightrickman@gmail.com>
2018-11-01 11:30:32 -0400

    switched to xhr

--- main.js
+++ main.js
@@ -5,33 +5,33 @@ $(document).ready(function () {
 		var value = $('input').val(),
 				url = "http://api.giphy.com/v1/gifs/search?q=" + value + "&api_key=9MZJw8INDF2Zb5NZyXYeRnE2dz4K6vfP&limit=5";
 
-		$.ajax({
-	    type: "GET",                                            // GET or POST
-	    url: url,                                               // Path to file
-	    timeout: 2000,                                          // Waiting time
-	    beforeSend: function() {                                // Before Ajax 
-	      $('body').append('<div id="load">Loading</div>');      // Load message
-	    },
-	    complete: function() {                                  // Once finished
-	      $('#load').remove();                                  // Clear message
-	    },
-	    success: function(data) {                               // Show content
-	    	console.log('Success!', data);
+		// $.ajax({
+	 //    type: "GET",                                            // GET or POST
+	 //    url: url,                                               // Path to file
+	 //    timeout: 2000,                                          // Waiting time
+	 //    beforeSend: function() {                                // Before Ajax 
+	 //      $('body').append('<div id="load">Loading</div>');      // Load message
+	 //    },
+	 //    complete: function() {                                  // Once finished
+	 //      $('#load').remove();                                  // Clear message
+	 //    },
+	 //    success: function(data) {                               // Show content
+	 //    	console.log('Success!', data);
 
-	      showGifs(data);
-	    },
-	    error: function(error) {                                     // Show error msg 
-	      console.warn(error);
-	    }
-	  });
+	 //      showGifs(data);
+	 //    },
+	 //    error: function(error) {                                     // Show error msg 
+	 //      console.warn(error);
+	 //    }
+	 //  });
 
-		// var xhr = $.getJSON("http://cors.io/?u=http://api.giphy.com/v1/gifs/search?q=" + value + "&api_key=9MZJw8INDF2Zb5NZyXYeRnE2dz4K6vfP&limit=5");
+		var xhr = $.get("http://cors.io/?u=http://api.giphy.com/v1/gifs/search?q=" + value + "&api_key=9MZJw8INDF2Zb5NZyXYeRnE2dz4K6vfP&limit=5");
 
-		// xhr.done(function(data) { 
-		// 	console.log('Success!', data);
+		xhr.done(function(data) { 
+			console.log('Success!', data);
 
-		// 	showGifs(data);
-		// });
+			showGifs(data);
+		});
 	});
 
 	function showGifs(data) {
